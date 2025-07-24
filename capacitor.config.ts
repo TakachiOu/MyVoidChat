2802443d036bbbe2c1457e6319ce0afe23264280
Takachi <you@example.com>
2025-07-24 14:45:54 +0100

    l3ziz

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,25 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
+import { KeyboardResize } from '@capacitor/keyboard'; 
 
 const config: CapacitorConfig = {
   appId: 'com.company.myvoidchat',
   appName: 'voidchat',
-  webDir: 'public'
+  webDir: 'public',
+  server: {
+    androidScheme: 'https',
+    allowNavigation: [
+      'myvoidchat.onrender.com',
+      'cdnjs.cloudflare.com'
+    ]
+  },
+  // ▼▼▼-- هذا هو القسم الجديد والمهم --▼▼▼
+  plugins: {
+    Keyboard: {
+      resize: KeyboardResize.Body, 
+      resizeOnFullScreen: true    
+    }
+  }
+  
 };
 
 export default config;
