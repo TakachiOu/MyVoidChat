2d3fcfc8e69fc47636a08f448520b3b1538803bc
Takachi <you@example.com>
2025-07-24 13:03:06 +0100

    Serve static files from public folder

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const fs = require('fs');
 const { Server } = require("socket.io");
 
 const app = express();
+app.use(express.static('public'));
 const server = http.createServer(app);
 const io = new Server(server);
 const PORT = process.env.PORT || 3000;
