2feaa14b125852c260d6abebfe41390fd05f5ea6
Abdou Ouassaa <abdououasaa@gmail.com>
2026-04-06 13:33:30 +0100

    1.6v

---
+++ android/app/src/main/java/com/takachi/chatchi/MainActivity.java
@@ -0,0 +1,5 @@
+package com.takachi.chatchi;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,25 +1,15 @@
 import { CapacitorConfig } from '@capacitor/cli';
-import { KeyboardResize } from '@capacitor/keyboard'; 
+
 
 const config: CapacitorConfig = {
-  appId: 'com.company.myvoidchat',
-  appName: 'Myvoidchat',
+  appId: 'com.takachi.chatchi',
+  appName: 'Chatchi',
   webDir: 'public',
   server: {
-    androidScheme: 'https',
-    allowNavigation: [
-      'myvoidchat.onrender.com',
-      'cdnjs.cloudflare.com'
-    ]
-  },
-  // ▼▼▼-- هذا هو القسم الجديد والمهم --▼▼▼
-  plugins: {
-    Keyboard: {
-      resize: KeyboardResize.Body, 
-      resizeOnFullScreen: true    
-    }
+    url: 'https://chatchi.onrender.com/',
+    androidScheme: 'https'
   }
-  
+
 };
 
 export default config;
